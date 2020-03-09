1e15b870562a19a6cec9f2839680dd1af645c591
jvanthooft <julian99@hotmail.nl>
2020-03-09 16:07:23 +0100

    State space matrix

--- Cit_par.py
+++ Cit_par.py
@@ -7,150 +7,156 @@ from math import pi, sin, cos
 
 # Stationary flight condition
 
-hp0    =       	     # pressure altitude in the stationary flight condition [m]
-V0     =             # true airspeed in the stationary flight condition [m/sec]
-alpha0 =             # angle of attack in the stationary flight condition [rad]
-th0    =             # pitch angle in the stationary flight condition [rad]
+hp0    = 1500      	  # pressure altitude in the stationary flight condition [m]
+V0     = 82.3            # true airspeed in the stationary flight condition [m/sec]
+alpha0 =  0.04           # angle of attack in the stationary flight condition [rad]
+th0    =  0.08           # pitch angle in the stationary flight condition [rad]
 
 # Aircraft mass
-m      =             # mass [kg]
+m      = 10000            # mass [kg]
 
-# aerodynamic properties
-e      =             # Oswald factor [ ]
-CD0    =             # Zero lift drag coefficient [ ]
-CLa    =             # Slope of CL-alpha curve [ ]
+#Definition returns the state space matrix for both the symmetric as asymmetric case
+def statespacematrix(hp0,V0,alpha0,th0):
 
-# Longitudinal stability
-Cma    =             # longitudinal stability [ ]
-Cmde   =             # elevator effectiveness [ ]
+    # aerodynamic properties (from table C)
+    e      = 0.8            # Oswald factor [ ]
+    CD0    = 0.04            # Zero lift drag coefficient [ ]
+    CLa    = 5.084            # Slope of CL-alpha curve [ ]
+
+    # Longitudinal stability (from table C)
+    Cma    = -0.5626            # longitudinal stability [ ]
+    Cmde   = -1.1642            # elevator effectiveness [ ]
 
 
-# Aircraft geometry
+    # Aircraft geometry
 
-S      = 30.00	            # wing area [m^2]
-Sh     = 0.2 * S            # stabiliser area [m^2]
-Sh_S   = Sh / S	            # [ ]
-lh     = 0.71 * 5.968       # tail length [m]
-c      = 2.0569	            # mean aerodynamic cord [m]
-lh_c   = lh / c	            # [ ]
-b      = 15.911	            # wing span [m]
-bh     = 5.791	            # stabiliser span [m]
-A      = b ** 2 / S         # wing aspect ratio [ ]
-Ah     = bh ** 2 / Sh       # stabiliser aspect ratio [ ]
-Vh_V   = 1	                # [ ]
-ih     = -2 * pi / 180      # stabiliser angle of incidence [rad]
+    S      = 30.00	            # wing area [m^2]
+    Sh     = 0.2 * S            # stabiliser area [m^2]
+    Sh_S   = Sh / S	            # [ ]
+    lh     = 0.71 * 5.968       # tail length [m]
+    c      = 2.0569	            # mean aerodynamic cord [m]
+    lh_c   = lh / c	            # [ ]
+    b      = 15.911	            # wing span [m]
+    bh     = 5.791	            # stabiliser span [m]
+    A      = b ** 2 / S         # wing aspect ratio [ ]
+    Ah     = bh ** 2 / Sh       # stabiliser aspect ratio [ ]
+    Vh_V   = 1	                # [ ]
+    ih     = -2 * pi / 180      # stabiliser angle of incidence [rad]
 
-# Constant values concerning atmosphere and gravity
+    # Constant values concerning atmosphere and gravity
 
-rho0   = 1.2250          # air density at sea level [kg/m^3] 
-lambdaa = -0.0065        # temperature gradient in ISA [K/m]
-Temp0  = 288.15          # temperature at sea level in ISA [K]
-R      = 287.05          # specific gas constant [m^2/sec^2K]
-g      = 9.81            # [m/sec^2] (gravity constant)
+    rho0   = 1.2250          # air density at sea level [kg/m^3]
+    lambdaa = -0.0065        # temperature gradient in ISA [K/m]
+    Temp0  = 288.15          # temperature at sea level in ISA [K]
+    R      = 287.05          # specific gas constant [m^2/sec^2K]
+    g      = 9.81            # [m/sec^2] (gravity constant)
 
-# air density [kg/m^3]  
-rho    = rho0 * power( ((1+(lambdaa * hp0 / Temp0))), (-((g / (lambdaa*R)) + 1)))
-W      = m * g            # [N]       (aircraft weight)
+    # air density [kg/m^3]
+    rho    = rho0 * np.power( ((1+(lambdaa * hp0 / Temp0))), (-((g / (lambdaa*R)) + 1)))
+    W      = m * g            # [N]       (aircraft weight)
 
-# Constant values concerning aircraft inertia
+    # Constant values concerning aircraft inertia
 
-muc    = m / (rho * S * c)
-mub    = m / (rho * S * b)
-KX2    = 0.019
-KZ2    = 0.042
-KXZ    = 0.002
-KY2    = 1.25 * 1.114
+    muc    = m / (rho * S * c)
+    mub    = m / (rho * S * b)
+    KX2    = 0.019
+    KZ2    = 0.042
+    KXZ    = 0.002
+    KY2    = 1.25 * 1.114
 
-# Aerodynamic constants
+    # Aerodynamic constants
 
-Cmac   = 0                      # Moment coefficient about the aerodynamic centre [ ]
-CNwa   = CLa                    # Wing normal force slope [ ]
-CNha   = 2 * pi * Ah / (Ah + 2) # Stabiliser normal force slope [ ]
-depsda = 4 / (A + 2)            # Downwash gradient [ ]
+    Cmac   = 0                      # Moment coefficient about the aerodynamic centre [ ]
+    CNwa   = CLa                    # Wing normal force slope [ ]
+    CNha   = 2 * pi * Ah / (Ah + 2) # Stabiliser normal force slope [ ]
+    depsda = 4 / (A + 2)            # Downwash gradient [ ]
 
-# Lift and drag coefficient
+    # Lift and drag coefficient
 
-CL = 2 * W / (rho * V0 ** 2 * S)              # Lift coefficient [ ]
-CD = CD0 + (CLa * alpha0) ** 2 / (pi * A * e) # Drag coefficient [ ]
+    CL = 2 * W / (rho * V0 ** 2 * S)              # Lift coefficient [ ]
+    CD = CD0 + (CLa * alpha0) ** 2 / (pi * A * e) # Drag coefficient [ ]
 
-# Stability derivatives
+    # Stability derivatives
 
-CX0    = W * sin(th0) / (0.5 * rho * V0 ** 2 * S)
-CXu    = -0.02792
-CXa    = +0.47966		# Positive! (has been erroneously negative since 1993) 
-CXadot = +0.08330
-CXq    = -0.28170
-CXde   = -0.03728
+    CX0    = W * sin(th0) / (0.5 * rho * V0 ** 2 * S)
+    CXu    = -0.02792
+    CXa    = +0.47966		# Positive! (has been erroneously negative since 1993)
+    CXadot = +0.08330
+    CXq    = -0.28170
+    CXde   = -0.03728
 
-CZ0    = -W * cos(th0) / (0.5 * rho * V0 ** 2 * S)
-CZu    = -0.37616
-CZa    = -5.74340
-CZadot = -0.00350
-CZq    = -5.66290
-CZde   = -0.69612
+    CZ0    = -W * cos(th0) / (0.5 * rho * V0 ** 2 * S)
+    CZu    = -0.37616
+    CZa    = -5.74340
+    CZadot = -0.00350
+    CZq    = -5.66290
+    CZde   = -0.69612
 
-Cmu    = +0.06990
-Cmadot = +0.17800
-Cmq    = -8.79415
+    Cmu    = +0.06990
+    Cmadot = +0.17800
+    Cmq    = -8.79415
 
-CYb    = -0.7500
-CYbdot =  0     
-CYp    = -0.0304
-CYr    = +0.8495
-CYda   = -0.0400
-CYdr   = +0.2300
+    CYb    = -0.7500
+    CYbdot =  0
+    CYp    = -0.0304
+    CYr    = +0.8495
+    CYda   = -0.0400
+    CYdr   = +0.2300
 
-Clb    = -0.10260
-Clp    = -0.71085
-Clr    = +0.23760
-Clda   = -0.23088
-Cldr   = +0.03440
+    Clb    = -0.10260
+    Clp    = -0.71085
+    Clr    = +0.23760
+    Clda   = -0.23088
+    Cldr   = +0.03440
 
-Cnb    =  +0.1348
-Cnbdot =   0     
-Cnp    =  -0.0602
-Cnr    =  -0.2061
-Cnda   =  -0.0120
-Cndr   =  -0.0939
+    Cnb    =  +0.1348
+    Cnbdot =   0
+    Cnp    =  -0.0602
+    Cnr    =  -0.2061
+    Cnda   =  -0.0120
+    Cndr   =  -0.0939
 
-#--------Symmetric equations of motion in the form of:  C1 * xdot + C2 * x + C3 * u     --------
+    #--------Symmetric equations of motion in the form of:  C1 * xdot + C2 * x + C3 * u     --------
 
-C1 = np.array([[(-2*muc*c)/(V0**2),0,0,0],
-[0,(CZadot-2*muc)*(c/V0),0,0],
-[0,0,-c/V0,0],
-[0,(Cmadot*c)/V0,0,(-2*muc*c**2 * KY2)/(V0**2)]])
+    C1 = np.array([[(-2*muc*c)/(V0**2),0,0,0],
+    [0,(CZadot-2*muc)*(c/V0),0,0],
+    [0,0,-c/V0,0],
+    [0,(Cmadot*c)/V0,0,(-2*muc*c**2 * KY2)/(V0**2)]])
 
-C2 = np.array([[CXu/V0,CXa,CZ0,CXq*(c/V0)],
-               [CZu/V0,CZa,-CX0,(CZq+2*muc)*(c/V0)],
-               [0,0,0,c/V0],
-               [Cmu/V0,Cma,0,(Cmq*c)/V0]])
+    C2 = np.array([[CXu/V0,CXa,CZ0,CXq*(c/V0)],
+                   [CZu/V0,CZa,-CX0,(CZq+2*muc)*(c/V0)],
+                   [0,0,0,c/V0],
+                   [Cmu/V0,Cma,0,(Cmq*c)/V0]])
 
-C3 = np.array([[CXde],
-               [CZde],
-               [0],
-               [Cmde]])
+    C3 = np.array([[CXde],
+                   [CZde],
+                   [0],
+                   [Cmde]])
 
-### State Space Symmetric for state vector [u,alpha,theta,q]
-A_sym = -np.linalg.inv(C1)*C2
-B_sym = -np.linalg.inv(C1)*C3
+    ### State Space Symmetric for state vector [u,alpha,theta,q]
+    A_sym = np.dot(-np.linalg.inv(C1),C2)
+    B_sym = np.dot(-np.linalg.inv(C1),C3)
 
-#---------Asymmetric equations of motion in the form of: D1 * xdot + D2 * x + D3 * u ----------------
+    #---------Asymmetric equations of motion in the form of: D1 * xdot + D2 * x + D3 * u ----------------
 
-D1 = np.array([[(CYbdot-2*mub)*(b/V0),0,0,0],
-               [0,-b/(2*V0),0,0],
-               [0,0,-2*mub*KX2*(b**2 / V0**2),2*mub*KXZ*(b**2 / V0**2)],
-               [Cnbdot*(b/V0),0,2*mub*KXZ*(b**2/V0**2),-2*mub*KZ2*(b**2 / V0**2)]])
+    D1 = np.array([[(CYbdot-2*mub)*(b/V0),0,0,0],
+                   [0,-b/(2*V0),0,0],
+                   [0,0,-2*mub*KX2*(b**2 / V0**2),2*mub*KXZ*(b**2 / V0**2)],
+                   [Cnbdot*(b/V0),0,2*mub*KXZ*(b**2/V0**2),-2*mub*KZ2*(b**2 / V0**2)]])
 
-D2 = np.array([[CYb,CL,(CYp*b)/(2*V0),(CYr-4*mub)*(b/(2*V0))],
-               [0,0,b/(2*V0),0],
-               [Clb,0,(Clp*b)/(2*V0),Clr*(b/(2*V0))],
-               [Cnb,0,(Cnp*b)/(2*V0),(Cnr*b)/(2*V0)]])
+    D2 = np.array([[CYb,CL,(CYp*b)/(2*V0),(CYr-4*mub)*(b/(2*V0))],
+                   [0,0,b/(2*V0),0],
+                   [Clb,0,(Clp*b)/(2*V0),Clr*(b/(2*V0))],
+                   [Cnb,0,(Cnp*b)/(2*V0),(Cnr*b)/(2*V0)]])
 
-D3 = np.array([[CYda,CYdr],
-               [0,0],
-               [Clda,Cldr],
-               [Cnda,Cndr]])
+    D3 = np.array([[CYda,CYdr],
+                   [0,0],
+                   [Clda,Cldr],
+                   [Cnda,Cndr]])
 
-### State Space Asymmetric for state vector [beta, phi, p, r]
-A_asym = -np.linalg.inv(D1)*D2
-B_asym = -np.linalg.inv(D1)*D3
+    ### State Space Asymmetric for state vector [beta, phi, p, r]
+
+    A_asym = np.dot(-np.linalg.inv(D1),D2)
+    B_asym = np.dot(-np.linalg.inv(D1),D3)
+
+    return A_sym,B_sym,A_asym,B_asym
