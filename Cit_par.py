2a4f06654e0f863477d57808b33b142781eb89da
n-v-amstel <niekvana@gmail.cm>
2020-03-09 11:58:42 +0100

    Added A and B matrices for symmetric and asymmetric state space

--- Cit_par.py
+++ Cit_par.py
@@ -43,7 +43,7 @@ ih     = -2 * pi / 180      # stabiliser angle of incidence [rad]
 # Constant values concerning atmosphere and gravity
 
 rho0   = 1.2250          # air density at sea level [kg/m^3] 
-lambdaa = -0.0065         # temperature gradient in ISA [K/m]
+lambdaa = -0.0065        # temperature gradient in ISA [K/m]
 Temp0  = 288.15          # temperature at sea level in ISA [K]
 R      = 287.05          # specific gas constant [m^2/sec^2K]
 g      = 9.81            # [m/sec^2] (gravity constant)
---
+++ State-Space.py
@@ -0,0 +1,9 @@
+import numpy as np
+import control as ctrl
+
+def State_Space_output():
+    A =
+    B =
+    C =
+    D =
+    sys = ctrl.ss(A,B,C,D)
