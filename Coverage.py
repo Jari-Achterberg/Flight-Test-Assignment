8f44b36927545a4f082284020bb1180d1b61a90a
Wessel <wesselskruin@gmail.com>
2020-03-25 11:15:28 +0100

    update

--- Coverage.py
+++ Coverage.py
@@ -6,24 +6,38 @@ from math import pi, sin, cos
 import cmath
 
 from Cit_par import statespacematrix
+hp0    = 1500       	     # pressure altitude in the stationary flight condition [m]
+V0 = 0.000000000000000000000000000001  # true airspeed in the stationary flight condition [m/sec]
+alpha0 =  0.04           # angle of attack in the stationary flight condition [rad]
+th0    =  0.08           # pitch angle in the stationary flight condition [rad]
+m      = 10000           # Aircraft mass
 
-statespacematrix(hp0,V0,alpha0,th0,m)
+print(statespacematrix(hp0,V0,alpha0,th0,m))
 
-
-from IAS_to_CAS import IAS_to_CAS
-
-IAS_to_CAS(V_IAS)
 
 
 from Verification import get_eigenvalues, get_coefficients_3x3_matrix,abcd_formula, abc_formula, get_short_period_eigenvalues,get_phugoid_eigenvalues,get_aperiodic_roll_eigenvalue,get_dutch_roll_eigenvalues, get_spiral_motion_eigenvalue, get_dutch_roll_with_aperiodic_roll_eigenvalues
 
-get_eigenvalues(A)
-get_coefficients_3x3_matrix(B)
-abcd_formula(a, b, c, d)
-abc_formula(a, b, c)
-get_short_period_eigenvalues(cz_alpha, cza_dot, mu_c, cz_q, cm_alpha, cm_alpha_dot, cm_q, ky)
-get_phugoid_eigenvalues(cx_u, cz_u, mu_c, cz_0)
-get_aperiodic_roll_eigenvalue(cl_p, mu_b, kx)
-get_dutch_roll_eigenvalues(mu_b, kz, cn_r, cy_beta, cn_beta)
-get_spiral_motion_eigenvalue(cl, cl_beta, cn_beta, cl_r, cn_r, cl_p, cy_beta, cn_p, mu_b)
-get_dutch_roll_with_aperiodic_roll_eigenvalues(mu_b, kx, kz, kxz, cl_r, cn_p, cn_r, cl_p, cl_beta, cn_beta)
+A= np.array([[2,-1,-1,0],
+             [-1,3,-1,-1],
+             [-1,-1,3,-1],
+             [0,-1,-1,2]])
+print(get_eigenvalues(A))    #should be 0,2,4   check
+
+#get_coefficients_3x3_matrix(B)
+
+abcd_formula(1, 0, -9, 0)  #x^{3}-9x=0 -> -3,03  check
+
+print(abc_formula(1, 2, -8))  #2,4  check
+
+print(get_short_period_eigenvalues(-1, 0, 0.5, 2, 3,0, -1,1)) #2,4  check
+
+print(get_phugoid_eigenvalues(-2, -4, 0.5, 2))  #2,4  check
+
+print(get_aperiodic_roll_eigenvalue(8, 2, 0.25))  #4  check
+
+print(get_dutch_roll_eigenvalues(1,1,-80,-80,-80))   #behaves as it should check
+
+print(get_spiral_motion_eigenvalue(2, 2, 1, 2, 2, 2, 0, 0, 0.25)) #4  check
+
+print(get_dutch_roll_with_aperiodic_roll_eigenvalues(2, 2, 1, 2, -80, -80, -80, -80, -80, -80))  #behaves as it should check
